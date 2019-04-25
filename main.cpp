80f8415c12f6574d94516fdb5bae3bbd837a4887
xjphongx <JimmyPhong16@csu.fullerton.edu>
2019-04-25 16:16:40 -0700

    adding skeleton code

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "linkedlist.cpp"
+
+using namespace std;
+
+void readFile(linkedlist & list, string & file) {
+
+}
+
+int main() {
+
+    return 0;
+}

dcc287684b7bf4cf8a693d2df7ae368cb9445df2
xjphongx <JimmyPhong16@csu.fullerton.edu>
2019-04-25 17:13:31 -0700

    working on readFile function

--- main.cpp
+++ main.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 #include <string>
 #include "linkedlist.cpp"
+#include <fstream>
+
 
 using namespace std;
 
 void readFile(linkedlist & list, string & file) {
+    ifstream inputFile;
+    inputFile.open(file);
+
+    while(!inputFile.eof())
+    {
+        node*temp;
+        temp = new node;
+        int num;
+        inputFile >> num;
+        list.addToFront(num);
+        delete temp;
+    }
+
+    
+
 
 }
 
 int main() {
-
+    
+    linkedlist list;
+    string file = "number.txt";
+    readFile(list,file);
+    
+    
     return 0;
 }
